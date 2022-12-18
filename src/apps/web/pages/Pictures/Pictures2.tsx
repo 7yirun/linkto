368c6eb772c8e14543b1246e21312563dcfc2cc1
zjx <523642558@qq.com>
2022-12-18 17:08:27 +0800

    完成图库搜索,关键词推荐

--- src/apps/web/pages/Pictures/Pictures2.tsx
+++ src/apps/web/pages/Pictures/Pictures2.tsx
@@ -3,7 +3,6 @@ import React, {useEffect, useState, useRef, SyntheticEvent} from "react";
 import Header from "apps/web/components/Header/Header"
 import {getSearchWords} from "../../../../service/service";
 import "./Picture.scss"
-import Icons from "lib/icons"
 import debounce from 'lodash/debounce.js'
 import {useWindowResize} from "apps/web/hooks";
 import {Dropdown} from "antd";
@@ -15,13 +14,14 @@ import AddToBookmark from "../../components/AddToBookmark/AddToBookmark";
 
 const Pictures = () => {
   const dispatch = useDispatch();
+  const searchState = useSelector<StateType, SearchStateType>(state => state.searchState)
 
   /*瀑布流相关----------------------------------------------------------start*/
   const clientWidth = useWindowResize();
-  const currentCols = Math.floor((clientWidth - 72) / 264);
-  const currentWidth = Math.floor((clientWidth - 24 * currentCols - 72) / currentCols);
+  const currentCols = useRef(Math.floor((clientWidth - 72) / 264))
+  const currentWidth = Math.floor((clientWidth - 24 * currentCols.current - 72) / currentCols.current);
   const layout = {
-    cols: currentCols,
+    cols: currentCols.current,
     colWidth: currentWidth
   }
   //请求到了哪一页
@@ -31,9 +31,17 @@ const Pictures = () => {
   //瀑布流每一列总高度
   const heightArrRef = useRef<number[]>( []);
   const SCROLL_GAP = 300
+
+  //如果是scroll触发的加载, 需要截住高频的load, 确保只请求一次
+  //true表示下滑可以加载, false表示下滑不加载
+  const scrollTriggerFlag = useRef(true);
+
+  //是否已经请求到了最后一页
+  const isLastPage = useRef(false);
+
   const loadImages = ()=>{
-    if(currentCols <=0){return}
-    heightArrRef.current = Array(currentCols).fill(0)
+    if(currentCols.current <=0){return}
+    heightArrRef.current = Array(currentCols.current).fill(0)
     for (let i = 0; i < imgList.length; i++) {
       //该元素排哪一列
       let colIndex = heightArrRef.current.findIndex((val, index) => {
@@ -50,36 +58,47 @@ const Pictures = () => {
     let scrollTop = window.pageYOffset;
     //屏幕高度
     let screenHeight = window.innerHeight;
-    console.log((shortestHeight - SCROLL_GAP <= scrollTop + screenHeight));
-    if (shortestHeight - SCROLL_GAP <= scrollTop + screenHeight) {
-      console.log('图不够');
-      console.log(pageNumRef.current);
-      queryImg({pageSize: 30, pageNum: pageNumRef.current++}, (res: any) => {
+    if (shortestHeight - SCROLL_GAP <= scrollTop + screenHeight && scrollTriggerFlag.current && !isLastPage.current) {
+      scrollTriggerFlag.current = false;
+
+      queryImg(searchState.confirmSearch ? {pageSize: 30, pageNum: pageNumRef.current++, description: searchState.description}:{pageSize: 30, pageNum: pageNumRef.current++}, (res: any) => {
+        if(res.data.pages < pageNumRef.current){
+          isLastPage.current = true;
+        }
         setImgList([...imgList, ...res.data.list])
+
       })
-    }
-  }
-  const getHeightArr = () => {
-    if (imgList.length <= 0) {
-      return
     }
   }
 
   useEffect(()=>{
     // 清空Header里的搜索栏
     dispatch(setDescription(''))
+    //获取热门推荐词
     getSearchWords({
       topNum: 5,
       word: ''
     },({data}:{data: string[]})=>{
       setKeywords(data)
     })
+    loadImages();
   }, [])
+  useEffect(()=>{
+    const handleScroll = loadImages
+    window.addEventListener('scroll', handleScroll);
+    return ()=>{
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [imgList, scrollTriggerFlag.current, isLastPage.current])
+  useEffect(()=>{
+    scrollTriggerFlag.current = true
+    //响应回来之前不可继续搜索
+    dispatch(setConfirmSearch(false));
+  }, [imgList])
 
   useEffect(() => {
+    currentCols.current = Math.floor((clientWidth - 72) / 264);
     loadImages()
-    //取出图片高度得到此时的图片高度数组
-    getHeightArr()
   }, [imgList, clientWidth])
   /*瀑布流相关----------------------------------------------------------end*/
 
@@ -112,8 +131,18 @@ const Pictures = () => {
   }
   /*显示大图--------------------------------end*/
 
+  /*搜索相关-----------------------------------------------------------start*/
   //搜索下方的推荐关键词
   const [keywords, setKeywords] = useState<string[]>([''])
+  useEffect(()=>{
+    //搜索时 重置一些状态
+    if(searchState.confirmSearch){
+      pageNumRef.current = 1;
+      setImgList([])
+      isLastPage.current = false;
+    }
+  }, [searchState.confirmSearch])
+  /*搜索相关-----------------------------------------------------------end*/
 
   return (
     <div className={'pictures'}>
