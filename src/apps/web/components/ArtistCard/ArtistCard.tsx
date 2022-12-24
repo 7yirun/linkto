5e7e2791b78558fb7e82f5f4bd4083374d009482
zjx <523642558@qq.com>
2022-12-24 21:35:50 +0800

    no message

--- src/apps/web/components/ArtistCard/ArtistCard.tsx
+++ src/apps/web/components/ArtistCard/ArtistCard.tsx
@@ -46,7 +46,7 @@ const ArtistCard = (props:any) => {
         props.canEditName &&
 		    <span className="edit" onClick={()=>{
           dispatch(setEditUser('nickname'))
-          history.push('/see-artist/sub-personalinfo/id=' + props.id)
+          history.push('/bookmark/sub-personalinfo')
         }}>
           编辑个人资料
             </span>
